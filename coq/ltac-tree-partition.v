0494aabb698c50feda93fc898918415eed5a7699
Kazuhiko Sakaguchi <pi8027@gmail.com>
2013-11-18 08:27:57 +0900

    ltac-tree-partition.v

--- coq/ltac-tree-partition.v
+++ coq/ltac-tree-partition.v
@@ -14,11 +14,11 @@ Proof. rewrite /unit; tauto. Qed.
 Lemma right_unit A : A <-> A /\ unit.
 Proof. rewrite /unit; tauto. Qed.
 
-Lemma partlemma1 A Al Ar B Bl Br :
+Lemma partitionL1 A Al Ar B Bl Br :
   (A <-> Al /\ Ar) -> (B <-> Bl /\ Br) -> (A /\ B <-> (Al /\ Bl) /\ (Ar /\ Br)).
 Proof. tauto. Qed.
 
-Lemma partlemma2 A A' B B' :
+Lemma partitionL2 A A' B B' :
   (A <-> A') -> (B <-> B') -> (A /\ B <-> A' /\ B').
 Proof. tauto. Qed.
 
@@ -30,28 +30,28 @@ Ltac decide_in x xs :=
   end.
 
 Ltac partition xs P :=
-  match P with
+  lazymatch P with
     | ?A /\ ?B =>
-      let Hl := fresh "H" in
-      let Hr := fresh "H" in
-      partition xs A => Hl;
-      partition xs B => Hr;
+      let Ha := fresh "H" in
+      let Hb := fresh "H" in
+      partition xs A => Ha;
+      partition xs B => Hb;
       match goal with
-        | Hl : _ <-> ?Al /\ ?Ar, Hr : _ <-> ?Bl /\ ?Br |- _ =>
-          let H0 := fresh "H" in
-          let H1 := fresh "H" in
+        | Ha : _ <-> ?Al /\ ?Ar, Hb : _ <-> ?Bl /\ ?Br |- _ =>
+          let Hl := fresh "H" in
+          let Hr := fresh "H" in
           match constr: (Al, Bl) with
             | (unit, _) => move: (iff_sym (left_unit Bl))
             | (_, unit) => move: (iff_sym (right_unit Al))
             | _ => move: (iff_refl (Al /\ Bl))
-          end; move => H0;
+          end; move => Hl;
           match constr: (Ar, Br) with
             | (unit, _) => move: (iff_sym (left_unit Br))
             | (_, unit) => move: (iff_sym (right_unit Ar))
             | _ => move: (iff_refl (Ar /\ Br))
-          end; move => H1;
-          move: (iff_trans (partlemma1 Hl Hr) (partlemma2 H0 H1)) =>
-            {Hl Hr H0 H1}
+          end; move => Hr;
+          move: (iff_trans (partitionL1 Ha Hb) (partitionL2 Hl Hr)) =>
+            {Ha Hb Hl Hr}
       end
     | ?X =>
       let inl := decide_in X xs in
@@ -62,6 +62,9 @@ Ltac partition xs P :=
   end.
 
 Goal False.
-  move: True True True True True True => A B C D E F.
-  partition [:: A; D] (A /\ B /\ (C /\ D /\ E) /\ F).
+  move: True True True True True True True True => A B C D E F G H.
+  partition [:: A; C; F] (A /\ (B /\ C /\ D) /\ E /\ (F /\ G) /\ H).
+  move => _.
+  partition [:: A; C; D; G] ((A /\ B /\ C) /\ D /\ (E /\ F /\ G) /\ H).
+  move => _.
 Abort.
