223d636713bafba2c60d0cedc5996239b9e212c3
Kazuhiko Sakaguchi <pi8027@gmail.com>
2014-03-09 17:15:07 +0900

    omega_benchmark.v

--- coq/omega_benchmark.v
+++ coq/omega_benchmark.v
@@ -1,7 +1,15 @@
 Require Import Omega.
 
-Notation minn x y := (x - (x - y)).
+Notation minn x y := (x - (x - y)) (only parsing).
+
+Lemma minnC x y : minn x y = minn y x.
+Proof. Time omega. Time Qed.
+
+Lemma minnC' x y : minn x y = minn (y - 0) (x - 0).
+Proof. Time omega. Time Qed.
+
+Lemma minnC'' x y : minn (x - 0) y = minn (y - 0) x.
+Proof. Time omega. Time Qed.
 
 Lemma minnA x y z : minn x (minn y z) = minn (minn x y) z.
-  omega.
-Qed.
+Proof. Time omega. Time Qed.
