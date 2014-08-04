d9e3ae6e11dc2c481891fad84a053bb96ddd0db7
Kazuhiko Sakaguchi <pi8027@gmail.com>
2014-08-04 13:19:29 +0900

    pumping

--- coq/pumping.v
+++ coq/pumping.v
@@ -166,16 +166,16 @@ Proof. by elim: n => //= n H; rewrite size_cat H mulSn. Qed.
 Lemma pumping_size (T : finType) (a : T) (L : nat -> Prop) :
   (forall k, exists x y : nat,
     k <= y /\ L (x + y) /\
-    forall u v, y = u + v -> v != 0 -> exists i, L (x + u + i * v) -> False) ->
+    forall u v, y = u + v.+1 -> exists i, L (x + u + i * v.+1) -> False) ->
   ~ regular (fun s : seq T => L (size s)).
 Proof.
   move => H; apply pumping => k; move: H => /(_ k) [x [y [H0 [H1 H]]]].
   exists (nseq x a), (nseq y a), [::]; rewrite !size_cat /= addn0 !size_nseq;
     do !split => //; move => {k H0 H1} u v w H0.
   case/(@nseq_eq_cat T): H0 (H0) H => <-; case/(@nseq_eq_cat T) => <- <-.
-  move: {u v w} (size u) (size v) (size w) => u v w.
-  move/(f_equal size); rewrite !size_cat !size_nseq => -> /(_ (u + w) v);
-    case: v => // v; rewrite -addnA (addnC v.+1) => /(_ erefl erefl) [i H] _.
+  move: {u v w} (size u) (size v) (size w) => u [] // v w /(f_equal size).
+  rewrite !size_cat !size_nseq => -> /(_ (u + w) v);
+    rewrite -addnA (addnC v.+1) => /(_ erefl) [i H] _.
   by exists i; rewrite !size_cat size_rep !size_nseq /= addn0
                        (addnC (_ * _)) !addnA -(addnA x).
 Qed.
@@ -187,7 +187,7 @@ Lemma primes_non_regular (T : finType) (a : T) :
 Proof.
   apply (pumping_size a prime) => k {T a}.
   case: (prime_above k.+1) => p; move/subnK => <-; move: (p - k.+2) => {p} p Hp.
-  exists p.+2, k; rewrite !addSnnS; do !split => //; move => u [] // v {Hp} _ _.
+  exists p.+2, k; rewrite !addSnnS; do !split => //; move => u v {Hp} _.
   exists (p.+2 + u); rewrite -mulnS; apply/negP/primePn; right; exists v.+2.
   - by rewrite ltnS /=; apply ltn_Pmull.
   - by rewrite dvdn_mull.
