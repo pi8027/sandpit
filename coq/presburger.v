0f33f51163ccb8ca2335e9d3d1a827832374164a
Kazuhiko Sakaguchi <pi8027@gmail.com>
2014-08-30 17:01:16 +0900

    presburger.v

--- coq/presburger.v
+++ coq/presburger.v
@@ -4,157 +4,29 @@
 
 Require Import
   ssreflect ssrfun ssrbool eqtype ssrnat seq choice fintype tuple finfun div
-  bigop ssralg ssrnum ssrint intdiv dfa_to_re regexp automata.
+  bigop ssralg ssrnum ssrint intdiv
+  dfa_to_re regexp automata.
 Import GRing.Theory Num.Theory.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
 Import Prenex Implicits.
 
-(* term and formula of Presburger arithmetic *)
-
-Inductive term (v : nat) :=
-  | t_const of nat
-  | t_var   of 'I_v
-  | t_add   of term v & term v
-  | t_mulc  of nat & term v.
-
-Inductive formula (v : nat) :=
-  | f_all    of formula v.+1
-  | f_exists of formula v.+1
-  | f_neg    of formula v
-  | f_and    of formula v & formula v
-  | f_or     of formula v & formula v
-  | f_leq    of term v & term v.
-
-(* normal form of Prethburger formula *)
-
-Inductive nformula (v : nat) :=
-  | nf_exists of nformula v.+1
-  | nf_neg    of nformula v
-  | nf_or     of nformula v & nformula v
-  | nf_atomic of int ^ v & int.
-
-(* semantics of Presburger arithmetic  *)
-
-Definition cons_tuple (A : Type) n (h : A) (t : A ^ n) : A ^ n.+1 :=
-  [ffun m =>
-   match m with
-     | Ordinal 0 Hm => h
-     | Ordinal m.+1 Hm =>
-       t (@Ordinal n m (eq_ind (m.+1 < n.+1) is_true Hm (m < n) (ltnS _ _)))
-   end].
-
-Fixpoint term_val v (t : term v) (i : nat ^ v) : nat :=
-  match t with
-    | t_const n => n
-    | t_var v => i v
-    | t_add t t' => term_val t i + term_val t' i
-    | t_mulc n t => n * term_val t i
-  end.
-
-Fixpoint formula_semantics v (f : formula v) : nat ^ v -> Prop :=
-  match f with
-    | f_all f =>
-      fun i => forall n : nat, formula_semantics f (cons_tuple n i)
-    | f_exists f =>
-      fun i => exists n : nat, formula_semantics f (cons_tuple n i)
-    | f_neg f => fun i => ~ formula_semantics f i
-    | f_and f f' => fun i => formula_semantics f i /\ formula_semantics f' i
-    | f_or f f' => fun i => formula_semantics f i \/ formula_semantics f' i
-    | f_leq t t' => fun i =>  term_val t i <= term_val t' i
-  end.
-
-Fixpoint nformula_semantics v (f : nformula v) : nat ^ v -> Prop :=
-  match f with
-    | nf_exists f =>
-      fun i => exists n : nat, nformula_semantics f (cons_tuple n i)
-    | nf_neg f => fun i => ~ nformula_semantics f i
-    | nf_or f f' => fun i => nformula_semantics f i \/ nformula_semantics f' i
-    | nf_atomic t n => fun i => (\sum_(m < v) t m * i m <= n)%R
-  end.
-
-(* normal form computation *)
-
-Fixpoint normal_t v (t : term v) : int ^ v * int :=
-  match t with
-    | t_const n => ([ffun _ => 0%R], (n : int))
-    | t_var var => ([ffun var' => (var == var' : int)], 0%R)
-    | t_add t t' =>
-      let: (c, n) := normal_t t in
-      let: (c', m) := normal_t t' in
-      ([ffun var => c var + c' var], n + m)%R
-    | t_mulc c t =>
-      let: (c', n) := normal_t t in
-      ([ffun var => (c : int) * c' var], (c : int) * n)%R
-  end.
-
-Fixpoint normal_f v (f : formula v) : nformula v :=
-  match f with
-    | f_all f => nf_neg (nf_exists (nf_neg (normal_f f)))
-    | f_exists f => nf_exists (normal_f f)
-    | f_neg f => nf_neg (normal_f f)
-    | f_and f f' => nf_neg (nf_or (nf_neg (normal_f f)) (nf_neg (normal_f f')))
-    | f_or f f' => nf_or (normal_f f) (normal_f f')
-    | f_leq t t' =>
-      let: (c, n) := normal_t t in
-      let: (c', m) := normal_t t' in
-      nf_atomic [ffun var => c var - c' var]%R (m - n)%R
-  end.
-
-(* correctness proof *)
-
-Lemma nt_correctness v (t : term v) i :
-  (term_val t i : int) =
-  (let (c, n) := normal_t t in \sum_(m < v) c m * i m + n)%R.
+Theorem well_founded_ltof (A : Type) (f : A -> nat) :
+  well_founded (fun n m => f n < f m).
 Proof.
-  elim: t i => /=.
-  - move => n i; rewrite -{1}(add0r (n : int)); f_equal.
-    apply big_ind => //.
-    + by move => x y <- <-.
-    + by move => ? _; rewrite ffunE mul0r.
-  - move => var i; rewrite addr0.
-    rewrite (bigID (eq_op^~ var)) /= -{1}(addr0 (i var : int)); f_equal.
-    + by rewrite big_pred1_eq ffunE eqxx mul1r.
-    + apply big_ind => //.
-      * by move => x y <- <-.
-      * by move => ? /negPf; rewrite ffunE eq_sym => -> /=; rewrite mul0r.
-  - move => t; case_eq (normal_t t) => c n _ IHt.
-    move => t'; case_eq (normal_t t') => c' m _ IHt' i.
-    rewrite PoszD IHt IHt' !addrA (addrAC _ n); do 2 f_equal.
-    by rewrite -big_split /=; apply eq_big => // var _; rewrite ffunE mulrDl.
-  - move => n t; case_eq (normal_t t) => c m H IH i.
-    rewrite PoszM IH mulrDr big_distrr /=.
-    by f_equal; apply eq_big => // var _; rewrite ffunE mulrA.
-Qed.
+  move => x.
+  move: {2}(f x) (leqnn (f x)) => n.
+  elim: n x => [ | n IHn ] x H; constructor => y H0.
+  - apply False_ind, notF.
+    rewrite -(ltn0 (f y)).
+    apply (leq_trans H0 H).
+  - apply IHn.
+    rewrite -ltnS.
+    apply (leq_trans H0 H).
+Defined.
 
-Lemma nf_correctness v (f : formula v) i :
-  (forall v (f : nformula v) i,
-    let P := nformula_semantics f i in ~ ~ P -> P) ->
-  (formula_semantics f i <-> nformula_semantics (normal_f f) i).
-Proof.
-  move => dne; move: v f i; refine (formula_ind _ _ _ _ _ _) => /=.
-  - move => v f IH i; split => H.
-    + by case => n; apply; apply IH, H.
-    + by move => n; apply IH, dne => H0; apply H; exists n.
-  - by move => v f IH i; split; case => n H; exists n; apply IH.
-  - by move => v f IH i; split => H H0; apply H, IH.
-  - move => v f IHf f' IHf' i; split.
-    + by case => H H0 []; apply; [apply IHf | apply IHf'].
-    + by move => H; split; [apply IHf | apply IHf'];
-        apply dne => H'; apply H; [left | right].
-  - by move => v f IHf f' IHf' i; split;
-      (case => H; [left; apply IHf | right; apply IHf']).
-  - move => v t t' i; rewrite -lez_nat !nt_correctness.
-    case_eq (normal_t t); case_eq (normal_t t') => /= c n _ c' m _.
-    rewrite -ler_subr_addr -addrA addrC -ler_subl_addr
-      (@big_morph _ _ -%R%R 0%R _ 0%R _ (@opprD _)) ?oppr0 // -big_split /=.
-    set x := BigOp.bigop _ _ _; set y := BigOp.bigop _ _ _.
-    suff ->: x = y by []; rewrite {}/x {}/y.
-    by apply eq_big => // var _; rewrite ffunE mulrDl mulNr.
-Qed.
-
-(* automata construction *)
+(* extensions for fintype *)
 
 Section Range.
 
@@ -188,7 +60,7 @@ Definition range_enum : seq range :=
      map Posz (match k with Posz k' => iota 0 k'.+1 | _ => [::] end)).
 
 Lemma range_enum_uniq : uniq range_enum.
-Proof. 
+Proof.
   rewrite pmap_sub_uniq // cat_uniq !map_inj_in_uniq;
     first (case: i => i'; case: k => k'; rewrite ?iota_uniq // andTb andbT).
   - by rewrite -all_predC /=; elim: map.
@@ -219,6 +91,155 @@ Canonical range_subFinType := Eval hnf in [subFinType of range].
 
 End Range.
 
+(* term and formula of Presburger arithmetic *)
+
+Inductive term (v : nat) :=
+  | t_const   of nat
+  | t_var     of 'I_v
+  | t_add     of term v & term v
+  | t_mulc    of nat & term v.
+
+Inductive formula (v : nat) :=
+  | f_all     of formula v.+1
+  | f_exists  of formula v.+1
+  | f_neg     of formula v
+  | f_and     of formula v & formula v
+  | f_or      of formula v & formula v
+  | f_leq     of term v & term v.
+
+(* normal form of Prethburger formula *)
+
+Inductive nformula (v : nat) :=
+  | nf_exists of nformula v.+1
+  | nf_neg    of nformula v
+  | nf_or     of nformula v & nformula v
+  | nf_atomic of int ^ v & int.
+
+(* semantics of Presburger arithmetic  *)
+
+Definition cons_tuple (A : Type) n (h : A) (t : A ^ n) : A ^ n.+1 :=
+  [ffun m =>
+   match m with
+     | Ordinal 0 Hm => h
+     | Ordinal m.+1 Hm =>
+       t (@Ordinal n m (eq_ind (m.+1 < n.+1) is_true Hm (m < n) (ltnS _ _)))
+   end].
+
+Fixpoint term_val fvs (t : term fvs) (assign : nat ^ fvs) : nat :=
+  match t with
+    | t_const n => n
+    | t_var v => assign v
+    | t_add t t' => term_val t assign + term_val t' assign
+    | t_mulc n t => n * term_val t assign
+  end.
+
+Fixpoint formula_semantics fvs (f : formula fvs) : nat ^ fvs -> Prop :=
+  match f with
+    | f_all f =>
+      fun assign => forall n : nat, formula_semantics f (cons_tuple n assign)
+    | f_exists f =>
+      fun assign => exists n : nat, formula_semantics f (cons_tuple n assign)
+    | f_neg f => fun assign => ~ formula_semantics f assign
+    | f_and f f' =>
+      fun assign => formula_semantics f assign /\ formula_semantics f' assign
+    | f_or f f' =>
+      fun assign => formula_semantics f assign \/ formula_semantics f' assign
+    | f_leq t t' => fun assign =>  term_val t assign <= term_val t' assign
+  end.
+
+Fixpoint nformula_semantics fvs (f : nformula fvs) : nat ^ fvs -> Prop :=
+  match f with
+    | nf_exists f =>
+      fun assign => exists n : nat, nformula_semantics f (cons_tuple n assign)
+    | nf_neg f => fun assign => ~ nformula_semantics f assign
+    | nf_or f f' =>
+      fun assign => nformula_semantics f assign \/ nformula_semantics f' assign
+    | nf_atomic t n => fun assign => (\sum_(m < fvs) t m * assign m <= n)%R
+  end.
+
+(* normal form computation *)
+
+Fixpoint normal_t fvs (t : term fvs) : int ^ fvs * int :=
+  (* a_1 x_1 + ... + a_n x_n + m *)
+  match t with
+    | t_const n => ([ffun => 0%R], (n : int))
+    | t_var v => ([ffun v' => (v == v' : int)], 0%R)
+    | t_add t t' =>
+      let: (cs, n) := normal_t t in
+      let: (cs', m) := normal_t t' in
+      ([ffun var => cs var + cs' var], n + m)%R
+    | t_mulc c t =>
+      let: (cs, n) := normal_t t in
+      ([ffun var => (c : int) * cs var], (c : int) * n)%R
+  end.
+
+Fixpoint normal_f fvs (f : formula fvs) : nformula fvs :=
+  match f with
+    | f_all f => nf_neg (nf_exists (nf_neg (normal_f f)))
+    | f_exists f => nf_exists (normal_f f)
+    | f_neg f => nf_neg (normal_f f)
+    | f_and f f' => nf_neg (nf_or (nf_neg (normal_f f)) (nf_neg (normal_f f')))
+    | f_or f f' => nf_or (normal_f f) (normal_f f')
+    | f_leq t t' =>
+      let: (cs, n) := normal_t t in
+      let: (cs', m) := normal_t t' in
+      nf_atomic [ffun var => cs var - cs' var]%R (m - n)%R
+  end.
+
+(* correctness proof *)
+
+Lemma nt_correctness fvs (t : term fvs) assign :
+  (term_val t assign : int) =
+  (let (c, n) := normal_t t in \sum_(m < fvs) c m * assign m + n)%R.
+Proof.
+  elim: t assign => /=.
+  - move => n assign; rewrite -{1}(add0r (n : int)); f_equal.
+    apply big_ind => //.
+    + by move => x y <- <-.
+    + by move => ? _; rewrite ffunE mul0r.
+  - move => v assign; rewrite addr0.
+    rewrite (bigID (eq_op^~ v)) /= -{1}(addr0 (assign v : int)); f_equal.
+    + by rewrite big_pred1_eq ffunE eqxx mul1r.
+    + apply big_ind => //.
+      * by move => x y <- <-.
+      * by move => ? /negPf; rewrite ffunE eq_sym => -> /=; rewrite mul0r.
+  - move => t; case_eq (normal_t t) => cs n _ IHt.
+    move => t'; case_eq (normal_t t') => cs' m _ IHt' i.
+    rewrite PoszD IHt IHt' !addrA (addrAC _ n); do 2 f_equal.
+    by rewrite -big_split /=; apply eq_big => // var _; rewrite ffunE mulrDl.
+  - move => c t; case_eq (normal_t t) => cs n H IH i.
+    rewrite PoszM IH mulrDr big_distrr /=.
+    by f_equal; apply eq_big => // var _; rewrite ffunE mulrA.
+Qed.
+
+Lemma nf_correctness fvs (f : formula fvs) assign :
+  (forall fvs (f : nformula fvs) assign,
+    let P := nformula_semantics f assign in ~ ~ P -> P) ->
+  (formula_semantics f assign <-> nformula_semantics (normal_f f) assign).
+Proof.
+  move => dne; move: fvs f assign; refine (formula_ind _ _ _ _ _ _) => /=.
+  - move => fvs f IH assign; split => H.
+    + by case => a; apply; apply IH, H.
+    + by move => a; apply IH, dne => H0; apply H; exists a.
+  - by move => fvs f IH assign; split; case => a H; exists a; apply IH.
+  - by move => fvs f IH assign; split => H H0; apply H, IH.
+  - move => fvs f IHf f' IHf' assign; split.
+    + by case => H H0 []; apply; [apply IHf | apply IHf'].
+    + by move => H; split; [apply IHf | apply IHf'];
+        apply dne => H'; apply H; [left | right].
+  - by move => fvs f IHf f' IHf' assign; split;
+      (case => H; [left; apply IHf | right; apply IHf']).
+  - move => fvs t t' assign; rewrite -lez_nat !nt_correctness.
+    case_eq (normal_t t); case_eq (normal_t t') => /= cs n _ cs' m _.
+    rewrite -ler_subr_addr -addrA addrC -ler_subl_addr
+      (@big_morph _ _ -%R%R 0%R _ 0%R _ (@opprD _)) ?oppr0 // -big_split /=.
+    set x := BigOp.bigop _ _ _; set y := BigOp.bigop _ _ _.
+    suff ->: x = y by []; rewrite {}/x {}/y.
+    by apply eq_big => // var _; rewrite ffunE mulrDl mulNr.
+Qed.
+
+(* automata construction *)
+
 Lemma lez_divL d m n : (0 < d -> m <= n * d -> m %/ d <= n)%Z%R.
 Proof.
   move => H H0; rewrite -(ler_pmul2r H); apply: (ler_trans _ H0).
@@ -226,23 +247,64 @@ Proof.
   by apply modz_ge0, lt0r_neq0.
 Qed.
 
-Section dfa_of_atomic_formula.
+Definition dfa_all A : dfa A :=
+  {| dfa_s := tt; dfa_fin x := true; dfa_trans x a := tt |}.
 
-Variables (v : nat) (c : int ^ v).
+Lemma dfa_all_correct A q w : dfa_accept (dfa_all A) q w.
+Proof. by elim: w q => /=. Qed.
 
-Section def.
+Section automata_construction.
+
+Variables (fvs : nat) (cs : int ^ fvs).
+
+Lemma word_of_assign_decreasing (a : nat ^ fvs) :
+  a != [ffun=> 0] ->
+  \sum_(i < fvs) [ffun i0 => a i0 %/ 2] i < \sum_(i < fvs) a i.
+Proof.
+  move => H; rewrite ltnNge; apply/negP => H0; move/eqP: H;
+    apply; apply/ffunP => /= i; rewrite ffunE; apply/eqP.
+  have/(leq_trans H0) {H0} :
+    \sum_(i < fvs) [ffun i' => a i' %/ 2] i <= (\sum_(i < fvs) a i) %/ 2 by
+    apply (big_rec2 (fun x y => is_true (x <= y %/ 2))) =>
+      //= {i} i x y _; rewrite ffunE !leq_divRL // mulnDl;
+      apply leq_add, leq_trunc_div.
+  rewrite leq_divRL // => H.
+  have {H}: \sum_(i < fvs) a i == 0 by
+    case: (BigOp.bigop _ _ _) H => // n; rewrite leqNgt mulSn !ltnS leq_pmulr.
+  by apply contraTT => H; rewrite (bigID (eq_op^~ i)) /= big_pred1_eq;
+    case: (a i) H.
+Qed.
+
+Definition word_of_assign : nat ^ fvs -> seq (bool ^ fvs) :=
+  Fix
+    (well_founded_ltof (fun (a : nat ^ fvs) => \sum_(i < fvs) a i))
+    (fun _ => seq (bool ^ fvs))
+    (fun a IH =>
+      (if a == [ffun => 0] as x return (a == [ffun => 0]) = x -> _
+       then fun _ => [::]
+       else fun H => [ffun i => odd (a i)] ::
+                     IH _ (word_of_assign_decreasing (negbT H)))
+      (erefl (a == [ffun => 0]))).
+
+Fixpoint assign_of_word (w : seq (bool ^ fvs)) : nat ^ fvs :=
+  match w with
+    | [::] => [ffun => 0]
+    | ch :: w => [ffun i => ch i + assign_of_word w i * 2]
+  end.
+
+Section dfa_of_atomic_formula_definition.
 
 Variable (n : int).
 
-Definition state_lb : int := Num.min n (- \sum_(i : 'I_v | 0 <= c i) c i)%R.
-Definition state_ub : int := Num.max n (- \sum_(i : 'I_v | c i <= 0) c i)%R.
+Definition state_lb : int := Num.min n (- \sum_(i : 'I_fvs | 0 <= cs i) cs i)%R.
+Definition state_ub : int := Num.max n (- \sum_(i : 'I_fvs | cs i <= 0) cs i)%R.
 
 Lemma afdfa_s_proof : (state_lb <= n <= state_ub)%R.
 Proof. by rewrite /state_lb /state_ub ler_minl ler_maxr lerr. Qed.
 
-Lemma afdfa_trans_proof (q : range state_lb state_ub) (ch : bool ^ v) :
+Lemma afdfa_trans_proof (q : range state_lb state_ub) (ch : bool ^ fvs) :
   (state_lb <=
-   ((int_of_range q - \sum_(i : 'I_v | ch i) c i) %/ 2)%Z <=
+   ((int_of_range q - \sum_(i : 'I_fvs | ch i) cs i) %/ 2)%Z <=
    state_ub)%R.
 Proof.
   case: q => /= q /andP [].
@@ -259,15 +321,20 @@ Proof.
     rewrite -ltrNge ltr_def => /andP [].
 Qed.
 
-Definition dfa_of_af (n : int) : dfa [finType of bool ^ v] :=
+Definition dfa_of_af : dfa [finType of bool ^ fvs] :=
   {| dfa_state := [finType of range state_lb state_ub];
      dfa_s := Range afdfa_s_proof;
      dfa_fin q := (0 <= q)%R;
      dfa_trans q ch := Range (afdfa_trans_proof q ch)
   |}.
 
-End def.
+End dfa_of_atomic_formula_definition.
 
+Lemma af_validP (n : int) :
+  reflect
+    (forall assign, nformula_semantics (nf_atomic cs n) assign)
+    (dfa_equiv (dfa_of_af n) (dfa_all _)).
+Proof.
+Abort.
 
-
-End dfa_of_atomic_formula.
+End automata_construction.
